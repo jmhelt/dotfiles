3b584621ebcd58719d1cc299d1f4f1153b3f0c6f
Jeff Helt <jmhelt@users.noreply.github.com>
2023-04-25 11:35:31 -0400

    Just use bash.

---
+++ install.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+function is_installed {
+    dpkg-query -W -f='${Status}' "$pkg" 2>/dev/null | grep "ok installed" &> /dev/null
+}
+
+function install_package {
+    sudo apt-get install -y "$1"
+}
+
+function install_packages {
+    for pkg in "$@"; do
+	is_installed "$pkg" || install_package "$pkg"
+    done
+}
+
+function link_dotfile {
+    ln -sf "$SCRIPT_DIR/$1" "$HOME/.$1"
+}
+
+function link_dotfiles {
+    for f in "$@"; do
+	link_dotfile "$f"
+    done
+}
+
+function install_emacsd {
+    if [[ -d ~/.emacs.d ]]; then
+	cd ~/.emacs.d
+	git pull -f origin master
+    else
+	git clone git@github.com:jmhelt/emacs.d.git ~/.emacs.d
+    fi
+}
+
+function install_spotify {
+    if [[ $(is_installed spotify_client) -eq 1 ]]; then
+	sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410
+	echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+	sudo apt-get update
+	sudo apt-get install -y spotify-client
+    fi
+}
+
+sudo apt-get update && sudo apt-get upgrade -y
+
+install_packages \
+    cscope \
+    emacs \
+    firefox \
+    g++ \
+    gcc \
+    gdb \
+    git \
+    valgrind \
+    vim \
+    wget
+
+install_emacsd
+install_spotify
+
+link_dotfiles \
+    gitconfig
